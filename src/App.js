94c9038a4993b91de60c7b69f178ebd8a213929d
Phuong Nguyen <85018054+nguyenlamthuyphuong25@users.noreply.github.com>
2022-10-03 11:28:25 +0700

    Update edit base file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
-import './App.css';
+import './_App.scss';
+import '../src/components/HomePage'
+import HomePage from '../src/components/HomePage';
+import Menu from './components/menu/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Menu />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  './Menu.scss';
+
+export default function Menu() {
+  return (
+    <>
+        <ul className='menu-container'>
+            <li>Tài khoản</li>
+            <li>Dịch vụ</li>
+            <li>Khuyến mãi</li>
+            <li>Tòa nhà</li>
+            <li>Khu vực</li>
+            <li>Cài đặt</li>
+        </ul>
+    </>
+  )
+}
